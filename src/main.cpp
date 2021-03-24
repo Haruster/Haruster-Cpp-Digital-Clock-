8dd68313d61d799b170cf2e6a0391ab313f3f3fb
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-03-24 09:20:15 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,3 +5,10 @@
 #include <windows.h>
 #include <time.h>
 
+using namespace std;
+
+char d0[5][3] = { 178, 178, 178,
+				  178, ' ', 178,
+				  178, ' ', 178,
+				  178, ' ', 178,
+				  178, 178, 178};
